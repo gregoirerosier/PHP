49776e6bcb767c506b7dff154c4304eb03356dd7
Gregoire Rosier <48044835+gregoirerosier@users.noreply.github.com>
2019-03-05 19:38:59 -0500

    Update Sessions.php

--- Sessions.php
+++ Sessions.php
@@ -39,3 +39,43 @@ echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
 
 </body>
 </html>
+
+// Modify Session
+
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// to change a session variable, just overwrite it 
+$_SESSION["favcolor"] = "yellow";
+print_r($_SESSION);
+?>
+
+</body>
+</html>
+
+
+//Destroy Session
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// remove all session variables
+session_unset(); 
+
+// destroy the session 
+session_destroy(); 
+?>
+
+</body>
+</html>
